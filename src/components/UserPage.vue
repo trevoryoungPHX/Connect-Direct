14911fc5b564dc1803de1441ca47c8bce3377abf
trevoryoungPHX <tcyoung4@asu.edu>
2018-01-21 11:15:43 -0700

    Initial Commit

---
+++ src/components/UserPage.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="User">
+    You have successfully logged in
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'User',
+  data() {
+    return {
+      msg: 'some new component, yo'
+    }
+  },
+  created() {
+    if (!this.$auth.check()) {
+      this.$router.push('/')
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
---
+++ src/main.js
@@ -0,0 +1,29 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueAxios from 'vue-axios'
+import axios from 'axios'
+
+Vue.use(VueAxios, axios)
+Vue.axios.defaults.baseURL = 'http://localhost:8000'
+
+Vue.config.productionTip = false
+Vue.router = router
+
+/* eslint-disable no-new */
+
+Vue.use(require('@websanova/vue-auth'), {
+  auth: require('@websanova/vue-auth/drivers/auth/bearer.js'),
+  http: require('@websanova/vue-auth/drivers/http/axios.1.x.js'),
+  router: require('@websanova/vue-auth/drivers/router/vue-router.2.x.js'),
+  rolesVar: 'type'
+})
+
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
